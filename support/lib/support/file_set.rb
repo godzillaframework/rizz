82ecf692ac899d7dce5e4eb279110687d7d55743
krishpranav <krisna.pranav@gmail.com>
2023-07-02 18:58:07 +0530

    support: file_set

--- support/lib/support/file_set.rb
+++ support/lib/support/file_set.rb
@@ -1,15 +1,14 @@
 module FileSet
     extend self
 
-    def glob(glob_pattern, file=nil)
-        glob_pattern = File.join(File.dirname(file_path), glob_pattern) if file_path
-        file_list = Dir.glob(glob_pattern).sort
-        file_list.each { |file| yield(file) }
-        file_list
+    def glob(glob_pattern, file_path=nil)
+      glob_pattern = File.join(File.dirname(file_path), glob_pattern) if file_path
+      file_list = Dir.glob(glob_pattern).sort
+      file_list.each{ |file| yield(file) }
+      file_list
     end
-
-    def glob_require(glob_pattern, file=nil)
-        glob_ = File.join(file)
-        glob_
+  
+    def glob_require(glob_pattern, file_path=nil)
+      glob(glob_pattern, file_path) { |f| require f }
     end
-end
+  end
