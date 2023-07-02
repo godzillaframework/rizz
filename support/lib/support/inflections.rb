91854aaa716cc2764b3d1068b44f046182f0c93f
krishpranav <krisna.pranav@gmail.com>
2023-07-02 19:09:36 +0530

    support: inflections

--- support/lib/support/inflections.rb
+++ support/lib/support/inflections.rb
@@ -9,5 +9,27 @@ module Rizz
             attr_reader :singulars
             attr_reader :uncountables
         end
+        
+        def self.clear(scope = :all)
+            case scope
+            when :all 
+                @plurals, @singulars, @uncountables = [], [], []
+            else
+            end
+        end
+
+        def demodulize(s)
+            s = s.to_s 
+            return s.demodulize if s.respond_to?(:demodulize)
+            s.gsub()
+        end
+
+        def pluralize(s)
+            s = s.to_s
+            return s.pluralize if s.respond_to?(:pluralize)
+            Inflections.pluralize 
+            result
+        end
+        
     end
 end
